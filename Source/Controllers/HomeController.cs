afab145bd60588cf3a532ab3be5129cea0de6a06
rrchirinosc <rrchirinos@gmail.com>
2018-04-13 13:36:39 +0200

    #XpandUrMusic first commit -- only a running skeleton

---
+++ Source/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using XpandUrMusic.Models;
+using XpandUrMusic.Infrastructure;
+using Microsoft.Extensions.Options;
+
+namespace XpandUrMusic.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationOptions ApplicationOptions { get; set; }
+        private string appID;
+        private string appSecret;
+
+        static void GetSettings()
+        {
+            
+        }
+
+        public  HomeController(
+            IOptions<ApplicationOptions> applicationOptions)
+        {
+            ApplicationOptions = applicationOptions.Value;
+
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Source/Infrastructure/ApplicationOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XpandUrMusic.Infrastructure
+{
+
+    public class ApplicationOptions
+    {
+        public string ApplicationName { get; set; }
+        public string SpotifyClientID { get; set; }
+        public string SpotifySecret { get; set; }
+    }
+}
