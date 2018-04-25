f42874b63d2bb5e4f628ada7bfc471f0f7713c1d
rrchirinosc <rrchirinos@gmail.com>
2018-04-25 14:28:49 +0200

    Added Artists container class to replace ParentArtistsDTO

---
+++ Source/DTO/ArtistsContainerDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XpandUrMusic.DTO
+{    public class ArtistsContainerDTO
+    {
+        public ArtistsDTO Artists { get; set; }
+    }
+}
