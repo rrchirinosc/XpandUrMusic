882d99269e6dd34a70986465f7575167cdb27383
rrcchirinosc <rrchirinosc@gmail.com>
2022-11-17 21:52:51 +0100

    - updated from .net core 2.0 to 3.1

--- Source/Startup.cs
+++ Source/Startup.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using XpandUrMusic.Infrastructure;
 
 namespace XpandUrMusic
@@ -25,12 +22,14 @@ namespace XpandUrMusic
             // get configuration
             services.Configure<ApplicationOptions>(options => Configuration.GetSection("ApplicationOptions").Bind(options));
 
-            services.AddMvc();
+
+            services.AddRazorPages();
             services.AddMemoryCache();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -42,12 +41,11 @@ namespace XpandUrMusic
             }
 
             app.UseStaticFiles();
+            app.UseRouting();
 
-            app.UseMvc(routes =>
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}");
             });
         }
     }
